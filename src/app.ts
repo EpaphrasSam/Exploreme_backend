038ed409512ffb99515407fe03af5aeaf0f395b9
EpaphrasSam <isinesam@gmail.com>
2024-05-01 01:15:45 +0000

    Added a logger package

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import * as dotenv from "dotenv";
+import morgan from "morgan";
 import express from "express";
 import cors from "cors";
 import auth from "./routes/auth";
 import index from "./routes/index";
 import { TokenVerification } from "./middlewares/authMiddleware";
-import { loggerMiddleware } from "./middlewares/loggerMiddleware";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 import { notFoundMiddleware } from "./middlewares/notFoundMiddleware";
 
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
-app.use(loggerMiddleware);
+app.use(morgan("combined"));
 
 app.post("/", index);
 
